74db6aae5ee4b6ee8fa686f1a01d75514fc6f927
Eric Weitz <eweitz@broadinstitute.org>
2020-08-19 23:04:33 -0400

    Omit full referrer if from DSP product

--- src/client.js
+++ src/client.js
@@ -1,7 +1,33 @@
 import {_} from './utils'
+import Config from './config'
 
+/**
+ * Removes full "referrer" in cases required by DSP policy
+ *
+ * If navigating from e.g.:
+ *
+ *    - A Terra workspace to another Terra page, or
+ *    - An SCP study to another SCP page
+ *
+ *  The "referrer" includes workspace name or study name.  DSP considers
+ *  these private information not suitable to log in Mixpanel.  So here we
+ *  strip referrer if the referring domain is for a DSP product.
+ *
+ * @param {Object} defaultProps
+ */
+function filterReferrer(defaultProps) {
+  const referringDomain = defaultProps['referring_domain'];
+  if (Config.DSP_PORTALS.includes(referringDomain)) {
+    delete defaultProps['referrer']
+  }
+  return defaultProps
+}
+
+/**
+ * Gets properties shared across DSP that are suitable to log in Mixpanel
+ */
 function getDefaultProperties() {
-  return _.info.properties();
+  return filterReferrer(_.info.properties())
 }
 
 export {getDefaultProperties}
--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
 var Config = {
   DEBUG: false,
-  LIB_VERSION: '0.0.1'
+  LIB_VERSION: '0.0.1',
+  DSP_PORTALS: [
+    // Fully-qualified domain names for production DSP web UIs
+    'terra.bio',
+    'singlecell.broadinstitute.org'
+  ]
 };
 
 export default Config;
